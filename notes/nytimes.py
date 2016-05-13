490fb0045ccb13729705c4aabac47d5ef6a4da89
AJ Dyka <ajdyka@Serenity.local>
2016-05-13 12:35:50 +0800

    saving my work

---
+++ notes/nytimes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# Import required libraries
+import sys
+
+# Start a counter and store the textfile in memory
+count = 0
+lines = sys.stdin.readlines()
+lines.pop(0)
+
+# For each line, find the sum of index 2 in the list.
+for line in lines:
+  count = count + int(line.strip().split(',')[2])
+
+print count
+
+### EOF ###
